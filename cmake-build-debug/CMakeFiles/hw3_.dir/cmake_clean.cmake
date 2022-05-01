a2aa00ba097a7e2e5452f08a22288a7cc7f3ca0b
nnnnnmmmmm092 <nnnnnmmmmm092@protonmail.com>
2022-05-01 12:38:17 +0300

    Initial CMakeProject commit

---
+++ cmake-build-debug/CMakeFiles/hw3_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3_.dir/main.cpp.o"
+  "hw3_"
+  "hw3_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw3_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hw3_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hw3_.
+# This may be replaced when dependencies are built.
