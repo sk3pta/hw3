32c5b3ebc2f457e6c9769b2bd25e397b04e86c8d
sk3pta <nnnnnmmmmm092@protonmail.com>
2022-05-15 00:35:13 +0300

    FIXED: 1. При добавлении студентов в группу, пропадал их курс. Оказалось, в конструкторах класса стуентов вообще забыли курс добавить. ADDED: 1. Сделал поля ФИО и курс студента публичными. добавил и проверил методы findBy(LastName/Name/Course) 2. Проверил перегрузку Group + Student. Работает.

--- Group.cpp
+++ Group.cpp
@@ -5,7 +5,8 @@
 #include <map>
 #include <utility>
 
-Group::Group(ID* id_manager, std::string name, size_t students_amount = 0, const std::vector<std::string> subjects = {}) {
+Group::Group(ID *id_manager, std::string name, size_t students_amount = 0,
+             const std::vector<std::string> subjects = {}) {
     this->group_id = id_manager->getGroupId();
     this->students_amount = students_amount;
     this->name = std::move(name);
@@ -14,7 +15,8 @@ Group::Group(ID* id_manager, std::string name, size_t students_amount = 0, const
 }
 
 
-Group::Group(ID *id_manager, std::string name,  size_t students_amount, const std::list<Student>& students, const std::vector<std::string> subjects) {
+Group::Group(ID *id_manager, std::string name, size_t students_amount, const std::list<Student> &students,
+             const std::vector<std::string> subjects) {
     this->group_id = id_manager->getGroupId();
     this->students_amount = students_amount;
     this->name = std::move(name);
@@ -33,15 +35,15 @@ void Group::addStudent(const Student &student) {
     this->students.push_back(new_student);
 }
 
-const Student &Group::getStudent(unsigned int student_id) {
-    for (const auto& student : this->students) {
+const Student &Group::getStudentbyId(unsigned int student_id) {
+    for (const auto &student: this->students) {
         if (student.getStudentId() == student_id) return student;
     }
 }
 
 std::ostream &operator<<(std::ostream &out, const Group &group) {
     out << "Group: " << group.name << " with ID : " << group.group_id << std::endl;
-    for (const auto &student : group.students) {
+    for (const auto &student: group.students) {
         std::cout << student;
     }
 
@@ -50,18 +52,17 @@ std::ostream &operator<<(std::ostream &out, const Group &group) {
 
 }
 
-std::ostream &Nice_Grades(std::ostream& out, const Group& group)
-{
+std::ostream &Nice_Grades(std::ostream &out, const Group &group) {
     out << "Students wtih good marks in  " << group.name << std::endl;
-    for (auto student : group.students) {
+    for (auto student: group.students) {
         std::map<std::string, unsigned int> grades = student.get_grades();
         int good_grades = 0;
-        for (auto subject : group.subjects) {
+        for (const auto &subject: group.subjects) {
             if (grades[subject] > 3) {
-                good_grades +=1;
+                good_grades += 1;
             }
         }
-        if (good_grades == group.subjects.size()-1 ){
+        if (good_grades == group.subjects.size() - 1) {
             out << student << std::endl;
         }
     }
@@ -78,3 +79,42 @@ Group &operator-(Group &group, const Student &student) {
     //
     return group;
 }
+
+std::vector<Student> Group::findByLastName(const std::string &_lastname) {
+    std::vector<Student> _students = {};
+
+    for (const auto &student: this->students) {
+        if (student.last_name == _lastname) _students.push_back(student);
+    }
+
+    return _students;
+
+
+}
+
+std::vector<Student> Group::findByName(const std::string &_name) {
+    std::vector<Student> _students = {};
+
+    for (const auto &student: this->students) {
+        if (student.name == _name) _students.push_back(student);
+    }
+
+    return _students;
+}
+
+std::vector<Student> Group::findByCourse(size_t _course) {
+    std::vector<Student> _students = {};
+
+    for (const auto &student: this->students) {
+        if (student.course == _course) _students.push_back(student);
+    }
+
+    return _students;
+}
+
+
+
+
+
+
+
