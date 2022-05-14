3d8092a3f670de43e9668685b2346c4ec8fd21c0
marginoon <45904292+marginoon@users.noreply.github.com>
2022-05-14 20:10:08 +0300

    сделал сортировку , проблема которую уже кидал в телеграмм

--- Group.cpp
+++ Group.cpp
@@ -5,17 +5,20 @@
 #include <map>
 #include <utility>
 
-Group::Group(ID *id_manager, std::string name,  size_t students_amount = 0) {
+Group::Group(ID* id_manager, std::string name, size_t students_amount = 0, const std::vector<std::string> subjects) {
     this->group_id = id_manager->getGroupId();
     this->students_amount = students_amount;
     this->students = {};
     this->name = std::move(name);
+    this->subjects = {};
 }
 
-Group::Group(ID *id_manager, std::string name,  size_t students_amount, const std::list<Student>& students) {
+
+Group::Group(ID *id_manager, std::string name,  size_t students_amount, const std::list<Student>& students, const std::vector<std::string> subjects) {
     this->group_id = id_manager->getGroupId();
     this->students_amount = students_amount;
     this->name = std::move(name);
+    this->subjects = subjects;
     for (auto student: students) {
         student.setGroup(this->name);
         this->students.push_back(student);
@@ -45,6 +48,29 @@ std::ostream &operator<<(std::ostream &out, const Group &group) {
     return out;
 
 
+}
+
+std::ostream &Nice_Grades(std::ostream& out, const Group& group)
+{
+    out << "Students wtih good marks in  " << group.name << std::endl;
+    for (auto student : group.students) {
+        for (auto subject : group.subjects) {
+            std::map<std::string, unsigned int> grades =  student.get_grades();
+            bool good_grades = false;
+            for (int i = 0; i < grades.size(); i++) {
+                if (grades[group.subjects[i]] > 3) {
+                    good_grades = true;
+                }
+                else {
+                    good_grades = false;
+                }
+            }
+            if (good_grades == true) {
+                out << student << std::endl;
+            }
+        }
+    }
+    return out;
 }
 
 Group &operator+(Group &group, const Student &student) {
