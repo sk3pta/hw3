58c7458029254d1b06cd2ef180fa439ff4babd87
marginoon <45904292+marginoon@users.noreply.github.com>
2022-05-19 00:14:45 +0300

    добавил все классы, все что смог

--- interface.cpp
+++ interface.cpp
@@ -4,24 +4,90 @@
 
 LOBBY lobby() {
     for(;;) {
-        std::cout << "-------------HELLO THERE-------------" << std::endl;
-        std::cout << "YOU HAVE MANY OPTIONS OF INTERACTION" << std::endl;
-        std::cout << " 1 to add group" << std::endl;
-        std::cout << " 2 to add student" << std::endl;
-        std::cout << " 99 to print all groups" << std::endl;
+        try {
+            std::cout << "-------------HELLO THERE-------------" << std::endl;
+            std::cout << "YOU HAVE MANY OPTIONS OF INTERACTION" << std::endl;
+            std::cout << " 1 to add group" << std::endl;
+            std::cout << " 2 to add student" << std::endl;
+            std::cout << " 99 to print all groups" << std::endl;
 
-        size_t input;
+            size_t input;
 
-        input = input_size_t();
-        std::cout << std::endl;
+            input = input_size_t();
+            std::cout << std::endl;
 
-        switch (input) {
-            case 1:
-                return LOBBY::ADD_GROUP;
-            case 2:
-                return LOBBY::ADD_STUDENT;
-            case 99:
-                return LOBBY::PRINT_GROUPS;
+            switch (input) {
+                case 1:
+                    return LOBBY::ADD_GROUP;
+                case 2:
+                    return LOBBY::ADD_STUDENT;
+                case 3:
+                    return LOBBY::SAVE;
+                case 4:
+                    return LOBBY::DOWNLOAD;
+                case 5:
+                    return LOBBY::PRINT_GROUPS;
+                case 6:
+                    return LOBBY::ST_W_G_M;
+                case 7:
+                    return LOBBY::ST_W_B_M;
+                case 8:
+                    return LOBBY::SORT_ST;
+                case 9:
+                    return LOBBY::EDIT_GROUPS;
+                case 10:
+                    return LOBBY::EDIT_STUDENTS;
+                case 99:
+                    return LOBBY::EXIT;
+                default:
+                    throw std::exception();
+            }
+        }
+        catch(std::exception& ex){
+            std::cout << "Repeat input" << std::endl;
+        }
+    }
+}
+STUDENT_PROFILE editing_students(){
+    for(;;){
+        try{
+            input = input_size_t();
+            std::cout << std::endl;
+            switch(input){
+                case 0:
+                    return ST_EDIT_ALL;
+                case 1:
+                    return ST_EDIT_MARKS;
+                case 2:
+                    return ST_EDIT_NAME;
+                case 3:
+                    return ST_EDIT_COURSE;
+                case 4:
+                    return ST_DELETE;
+            }
+        }
+        catch(std::exception& ex){
+            std::cout << "Repeat input" << std::endl;
+        }
+    }
+}
+STUDENT_PROFILE editing_groups(){
+    for(;;){
+        try{
+            input = input_size_t();
+            std::cout << std::endl;
+            switch(input){
+                case 0:
+                    return GR_EDIT_COURSE;
+                case 1:
+                    return GR_EDIT_NAME;
+                case 2:
+                    return GR_DELETE;
+
+            }
+        }
+        catch(std::exception& ex){
+            std::cout << "Repeat input" << std::endl;
         }
     }
 }
