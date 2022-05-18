680358e535c4b50069400038f5ff7103935c994f
sk3pta <nnnnnmmmmm092@protonmail.com>
2022-05-18 23:25:19 +0300

    починил. чекни 85 строку в main.cpp

--- datamanager.h
+++ datamanager.h
@@ -17,20 +17,42 @@ public:
     void addGroup(ID *id_manager, std::string name, unsigned int course,
                   const std::vector<std::string> &subjects = {}, size_t students_amount = 0) {
         Group group(id_manager,name,course,subjects,students_amount);
-        groups_ids[group.getGroupbyId()] = group;
+        //groups_ids[group.getGroupbyId()] = group;
         groups.push_back(group);
     }
+
+    void addStudent(std::string name, ID *id_manager, std::string middle_name, std::string last_name,
+                    unsigned int course,
+                    std::map<std::string, unsigned int> grades , unsigned int group_id) {
+
+        for (auto & grp : groups ) {
+            if (grp.group_id == group_id) {
+                Student student(name, id_manager, middle_name, last_name, course, grades);
+                grp.addStudent(student);
+            }
+        }
+    }
+
+
+    /*
     void addStudent(std::string name, ID *id_manager, std::string middle_name, std::string last_name,
                     unsigned int course,
                     std::map<std::string, unsigned int> grades , unsigned int group_id){
         Student student(name, id_manager, middle_name, last_name, course, grades);
         students_ids[student.getStudentId()] = student;
         groups_ids[group_id]+student;
-    }
+        //auto ngr = getGroupUsingID(group_id);
+        //ngr = ngr + student;
 
-    Group getGroupUsingID(unsigned int id) {
-        for (const auto& group : groups_ids) {
-            if (group.first == id) return group.second;
+
+    }
+     */
+
+
+
+    Group& getGroupUsingID(unsigned int id) {
+        for ( auto& group : groups) {
+            if (group.group_id == id) return group;
         }
     }
 };
